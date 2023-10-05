250f6491d30e5eaf418cdb686b744a941beccbfe
Fensingpng <124172712+Fensingpng@users.noreply.github.com>
2023-10-05 21:22:39 +0300

    Add files via upload

--- 43c.pro
+++ 43c.pro
@@ -1,4 +1,5 @@
-QT       += core gui
+QT       += core gui printsupport
+
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -17,10 +18,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    qcustomplot.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    qcustomplot.h
 
 FORMS += \
     mainwindow.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,36 +1,62 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QFile>
-#include <QDebug>
+#include "qcustomplot.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Создание объекта CustomPlot
+    QCustomPlot *customPlot = new QCustomPlot(this);
+
+    // Чтение данных из файла PCM
+    QFile file("C:/qtt/43 c++/Часть 1.pcm");
+    file.open(QIODevice::ReadOnly);
+    QByteArray rawData = file.readAll();
+    file.close();
+
+    // Размер сэмпла в байтах
+    int byteSize = sizeof(qint16);
+
+    // Количество сэмплов
+    int sampleCount = rawData.size() / byteSize;
+
+    // Создание временной шкалы и сигнала на основе данных PCM
+    QVector<double> time(sampleCount);
+    QVector<double> signal(sampleCount);
+
+    for(int i = 0; i < sampleCount; i++) {
+        // Чтение сэмпла из данных PCM
+        qint16 sample = *reinterpret_cast<const qint16*>(rawData.constData() + i * sizeof(qint16));
+
+        // Вычисление времени для каждого сэмпла
+        time[i] = i;
+
+        // Преобразование сэмпла в сигнал (если необходимо)
+        signal[i] = sample;
+    }
+
+    // Установка данных на графике
+    customPlot->addGraph();
+    customPlot->graph(0)->setData(time, signal);
+
+    // Управление масштабированием и перемещением графика
+    customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+    // Оси координат
+    customPlot->xAxis->setLabel("Time");
+    customPlot->yAxis->setLabel("Signal");
+
+    // Отображение графика
+    customPlot->replot();
+
+    // Добавление графика на главное окно
+    setCentralWidget(customPlot);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-void MainWindow::on_pushButton_clicked()
-{
-    QString filePath = "C:/qtt/43 c++/43c/12333.pcm";
-    QFile file(filePath);
-
-    if (!file.open(QIODevice::ReadOnly)) {
-        qDebug() << "Failed to open file";
-        return;
-    }
-
-    QByteArray pcmData = file.readAll();
-    file.close();
-
-
-    // Пример вывода информации о PCM данных
-    qDebug() << "PCM data size:" << pcmData.size();
-}
-
-
