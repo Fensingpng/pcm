5fd1455bed9ff63c28221da780a69bc8f1369661
Fensingpng <124172712+Fensingpng@users.noreply.github.com>
2023-10-05 18:39:20 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,63 +1,11 @@
-#include <QCoreApplication>
-#include <QFile>
-#include <QtEndian>
-#include <QDebug>
+#include "mainwindow.h"
 
-#include <mem.h>
-
-void readWav(const QString filename) {
-    QFile file(filename);
-    if (!file.open(QIODevice::ReadOnly)) {
-        return;
-    }
-    quint32 chunkDataSize = 0;
-    QByteArray temp_buff;
-    char buff[0x04];
-    while (true) {
-        QByteArray tmp = file.read(0x04);
-        temp_buff.append(tmp);
-        int idx = temp_buff.indexOf("data");
-        if (idx >= 0) {
-            int lenOfData = temp_buff.length() - (idx + 4);
-            memcpy(buff, temp_buff.constData() + idx + 4, lenOfData);
-            int bytesToRead = 4 - lenOfData;
-
-            if (bytesToRead > 0) {
-                int read = file.read(buff + lenOfData, bytesToRead);
-                if (bytesToRead != read) {
-                    qDebug() << "something awful happens";
-                    return;
-                }
-            }
-            chunkDataSize = qFromLittleEndian<quint32>((const uchar*)buff);
-            break;
-        }
-        if (temp_buff.length() >= 8) {
-            temp_buff.remove(0, 0x04);
-        }
-    }
-    if (!chunkDataSize) {
-        qDebug() << "chunk data not found";
-        return;
-    }
-    qDebug() << "length of chunks =" << chunkDataSize;
-    int samples = 0;
-    while (file.read(buff, 0x04) > 0) {
-        chunkDataSize -= 4;
-        ++samples;
-        qint16 sampleCannel1 = qFromLittleEndian<qint16>((const uchar*)buff);
-        qint16 sampleCannel2 = qFromLittleEndian<qint16>((const uchar*)buff);
-
-        if (chunkDataSize == 0 || chunkDataSize & 0x80000000) {
-            break;
-        }
-    }
-    qDebug() << "readed" << samples << "samples";
-}
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    readWav("C:/Users/ovsya/OneDrive/Рабочий стол/123/124.wav");
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,4 +14,23 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString filePath = "C:/qtt/43 c++/43c/12333.pcm";
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Failed to open file";
+        return;
+    }
+
+    QByteArray pcmData = file.readAll();
+    file.close();
+
+
+    // Пример вывода информации о PCM данных
+    qDebug() << "PCM data size:" << pcmData.size();
+}
+
 
